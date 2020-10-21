12ea927d48c0f6a98ed228712a57fa64a508d97e
jkuehner <jochen.kuehner@kardex.com>
2020-10-21 08:26:26 +0200

    fix space handling

--- src/DomHelper.ts
+++ src/DomHelper.ts
@@ -1,58 +1,58 @@
 export class DomHelper {
-  static * getAllChildNodes(element: ParentNode, includeShadowDom = false, ignore?: Node[]): IterableIterator<Node> {
-    if (!ignore || ignore.indexOf(<Node><any>element) < 0) {
-      if (element.children) {
-        for (const node of element.children) {
-          yield node;
-          if (includeShadowDom && node.shadowRoot != null) {
-            yield node.shadowRoot;
-            const childs = DomHelper.getAllChildNodes(node.shadowRoot, includeShadowDom, ignore);
-            for (const cnode of childs) {
-              yield cnode;
+    static * getAllChildNodes(element: ParentNode, includeShadowDom = false, ignore?: Node[]): IterableIterator<Node> {
+        if (!ignore || ignore.indexOf(<Node><any>element) < 0) {
+            if (element.children) {
+                for (const node of element.children) {
+                    yield node;
+                    if (includeShadowDom && node.shadowRoot != null) {
+                        yield node.shadowRoot;
+                        const childs = DomHelper.getAllChildNodes(node.shadowRoot, includeShadowDom, ignore);
+                        for (const cnode of childs) {
+                            yield cnode;
+                        }
+                    }
+                    const childs = DomHelper.getAllChildNodes(node, includeShadowDom, ignore);
+                    for (const cnode of childs) {
+                        yield cnode;
+                    }
+                }
             }
-          }
-          const childs = DomHelper.getAllChildNodes(node, includeShadowDom, ignore);
-          for (const cnode of childs) {
-            yield cnode;
-          }
+            if (includeShadowDom && (<any>element).shadowRoot != null) {
+                yield (<any>element).shadowRoot;
+                const childs = DomHelper.getAllChildNodes((<any>element).shadowRoot, includeShadowDom);
+                for (const cnode of childs) {
+                    yield cnode;
+                }
+            }
         }
-      }
-      if (includeShadowDom && (<any>element).shadowRoot != null) {
-        yield (<any>element).shadowRoot;
-        const childs = DomHelper.getAllChildNodes((<any>element).shadowRoot, includeShadowDom);
-        for (const cnode of childs) {
-          yield cnode;
+        return null;
+    }
+
+    static removeAllChildnodes(node: Node, className?: string) {
+        if (!className) {
+            for (let c = node.firstChild; c !== null; c = node.firstChild) {
+                node.removeChild(c);
+            }
+        } else {
+            const elements = (<HTMLElement>node).querySelectorAll('.' + className);
+            for (const e of elements) {
+                if (e.parentNode == node)
+                    node.removeChild(e);
+            }
         }
-      }
     }
-    return null;
-  }
 
-  static removeAllChildnodes(node: Node, className?: string) {
-    if (!className) {
-      for (let c = node.firstChild; c !== null; c = node.firstChild) {
-        node.removeChild(c);
-      }
-    } else {
-      const elements = (<HTMLElement>node).querySelectorAll('.' + className);
-      for (const e of elements) {
-        if (e.parentNode == node)
-          node.removeChild(e);
-      }
-    }
-  }
-
-  static nodeIndex(node: Node) {
-    let i = 0;
-    while ((node = node.previousSibling) != null)
-      i++;
-    return i;
-  }
-
-  static childIndex(node: Node) {
-    for (let i = 0; i < node.parentElement.children.length; i++)
-      if (node.parentElement.children[i] == node)
+    static nodeIndex(node: Node) {
+        let i = 0;
+        while ((node = node.previousSibling) != null)
+            i++;
         return i;
-    return -1;
-  }
+    }
+
+    static childIndex(node: Node) {
+        for (let i = 0; i < node.parentElement.children.length; i++)
+            if (node.parentElement.children[i] == node)
+                return i;
+        return -1;
+    }
 }
