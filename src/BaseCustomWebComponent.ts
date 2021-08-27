a057034e3342eaca4e4fc1241bc2aadce259ddaf
Sven Knoerzer <sven.knoerzer@kardex.com>
2021-08-27 09:32:39 +0200

    Handle events like in a polymer (no automatic camel case name change) except TypedEvent

--- src/BaseCustomWebComponent.ts
+++ src/BaseCustomWebComponent.ts
@@ -107,8 +107,9 @@ export class BaseCustomWebComponentNoAttachedTemplate extends HTMLElement {
                         if (a.name == "@touch:contextmenu")
                             addTouchFriendlyContextMenu(node, this[a.value].bind(this));
                         else {
-                            if (node[a.name] instanceof TypedEvent) {
-                                (<TypedEvent<any>>node[a.name]).on(this[a.value].bind(this));
+                            let nm = a.name.substr(1).replace(/-([a-z])/g, (g) => g[1].toUpperCase());
+                            if (node[nm] instanceof TypedEvent) {
+                                (<TypedEvent<any>>node[nm]).on(this[a.value].bind(this));
                             } else {
                                 node.addEventListener(a.name, this[a.value].bind(this));
                             }
