c9a6cf4cdf6cd7b73e0688c68caebd076febd383
Jochen Kühner <jochen.kuehner@gmx.de>
2021-10-23 20:03:01 +0200

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "./BaseCustomWebComponent.js";
 export * from "./DomHelper.js";
 export * from "./LazyLoader.js";
 export * from "./TypedEvent.js";
+export * from "./TouchContextMenu.js";
